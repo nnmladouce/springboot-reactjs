686a0211e7778a727063b8601ba5c1e32e489dc3
Raissa Takoudjou Kengne <nnmladouce@yahoo.com>
2022-05-02 14:44:33 +0200

    some releases

--- customerapi/src/main/java/de/ladouce/customerapi/repository/CustVIPRepository.java
+++
@@ -1,7 +0,0 @@
-package de.ladouce.customerapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import de.ladouce.customerapi.entity.CustVPI;
-
-public interface CustVIPRepository extends JpaRepository<CustVPI,Long> {
-}
