877a41af125d322c68e82d07ec8a58ad0f571145
rtakoudjou <rt@bohnen.it>
2023-01-24 12:54:43 +0100

    project updated

--- customerapi/src/main/java/de/ladouce/customerapi/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package de.ladouce.customerapi.repository;
-
-import de.ladouce.customerapi.entity.Post;
-
-public interface PostRepository {
-    public String getAllPosts();
-    public String getAllPostsByLatLon(String lat, String lon);
-    public Post createPost(Post post);
-}
